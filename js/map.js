015eaa21b086a9b6cca289c6fcff721b9b8b3db5
vincent <541890086@qq.com>
2018-03-02 13:01:19 +0800

    sixth

---
+++ js/map.js
@@ -0,0 +1,4 @@
+/**
+ * Created by 55454 on 2018/3/2.
+ */
+(function(){ window.BMap_loadScriptTime = (new Date).getTime(); document.write('<script type="text/javascript" src="http://api.map.baidu.com/getscript?v=2.0&ak=4ca16f3d1df25593713c88206ae09189&services=&t=20180201111639"></script>');})();
